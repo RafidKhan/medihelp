b8e53e644161204eb056873257479e199df8f900
RafidKhan <rafidkhan321.rk@gmail.com>
2023-02-19 21:00:51 +0600

    config: defining color palette

---
+++ lib/utils/styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff5D737E);
+const kSecondaryColor = Color(0xff64B6AC);
+const kCardColor = Color(0xffC0FDFB);
+const kCardColorLite = Color(0xffDAFFEF);
+const kBackgroundColor = Color(0xffFCFFFD);
